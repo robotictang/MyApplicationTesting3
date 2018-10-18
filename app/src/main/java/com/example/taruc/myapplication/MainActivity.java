aa27c7e88d1c8451030988d35553875d3b10511b
robotictang <tang.tiong.yew@monash.edu>
2018-10-18 14:06:59 +0800

    test 2

--- app/src/main/java/com/example/taruc/myapplication/MainActivity.java
+++ app/src/main/java/com/example/taruc/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Activity Tang","Log information");
+        Log.d("Activity Tang","Log information 2");
     }
 }
